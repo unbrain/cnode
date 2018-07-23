87e6989e2363f6890efdea066740d4aa363dcee1
unbrain <marsorsun@gmail.com>
2018-07-23 20:35:57 +0800

    npm run build

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/cnode/dist',
     proxyTable: {},
 
     // Various Dev Server settings
